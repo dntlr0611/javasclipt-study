10dc661d5514c7625313e4d2d4b2c9801057ad87
Plant_cho <62693800+dntlr0611@users.noreply.github.com>
2022-07-21 12:01:02 +0900

    Create Test01_Stream.java

---
+++ spring_study/demo/src/main/java/TEST/Test01_Stream.java
@@ -0,0 +1,36 @@
+package TEST;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+public class Test01_Stream{
+    private static HashMap<String, ArrayList<String>> map = new HashMap<>();
+
+    public static void main(String[] args){
+        
+        String[] data = new String[]{"이건,010-5042-2100","이건,010-5042-2200","조우식,010-4184-4212","송유정,010-5042-2300"};
+        Stream<String> stream = Arrays.stream(data);
+        System.out.println(stream);
+        stream.forEach(a ->{
+            String name = a.split(",")[0];
+            String number = a.split(",")[1];
+            add(name, number);
+        });
+        System.out.println(map);
+        System.out.println(map.keySet());
+        System.out.println(map.get("이건"));
+    }
+     public static void add(String name, String number){
+        if(map.containsKey(name)){
+            map.get(name).add(number);
+        }else{
+            ArrayList<String> dataNumber = new ArrayList<>();
+            dataNumber.add(number);
+            map.put(name, dataNumber);
+        }
+    }
+}
+
+
